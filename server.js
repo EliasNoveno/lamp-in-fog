c5bf2684648ec048157bda09d7ed5a80bb8f065a
Yukiz <whiletrue@atomicmail.io>
2026-06-27 13:00:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { Pool } = require('pg');
-const const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const https = require('https');
 const crypto = require('crypto');
 const helmet = require('helmet');
